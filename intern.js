afd9d3972df047c77eb2dde81475d80b70027db5
deanthedream80 <dunningdean@gmail.com>
2022-03-31 12:03:24 -0500

    Changes made to intern.js to make other js files

--- intern.js
+++ intern.js
@@ -4,26 +4,6 @@ let i = document.getElementById("intern");
 
 i.addEventListener("click", myFunction);
 
-let addon = document.createElement("div");
-addon.innerText = "Intern, 2003-2005";
-addon.style.color = "black";
-addon.style.visibility = "visible";
-
-function toggle(e) {
-    if (e.style.visibility === "hidden") {
-        e.style.visibility = "visible";
-    }
-    if (e.style.visibility === "visible") {
-        e.style.visibility = "hidden";
-    }
-}
-
-let created = false;
 function myFunction() {
-    if (!created) {
-    i.append(addon);
-    created = true;
-    return;
-    }
-    toggle(addon)
-};
+  document.getElementById("intern").innerHTML = 'Middle Tennessee Christian Foundation<br><div style="color: black";>Intern, 2003-2005</div>';
+}
