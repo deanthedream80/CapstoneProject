ad9cc563d86a6ebbd8e7bd61673ab139bd6af663
deanthedream80 <dunningdean@gmail.com>
2022-03-31 11:47:38 -0500

    changes to index, intern, and mtsu

--- mtsu.js
+++ mtsu.js
@@ -5,5 +5,5 @@ let m = document.getElementById("mtsu");
 m.addEventListener("click", myFunction);
 
 function myFunction() {
-  document.getElementById("mtsu").innerHTML = `Middle Tennessee State University, B.S. Liberal Studies, 2003<br><div style="color: black";>Studied Anthropology and Psychology with a minor in Religious Studies</div>`;
+  document.getElementById("mtsu").innerHTML = 'Middle Tennessee State University, B.S. Liberal Studies, 2003<br><div style="color: black";>Studied Anthropology and Psychology with minor in Religious Studies</div>';
 }
